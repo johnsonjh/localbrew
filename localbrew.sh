dd21e1917132ebf8f2ab5d1a0267a54d8373eb03
Jeffrey H. Johnson <trnsz@pobox.com>
2022-07-23 17:16:51 -0400

    Initial import

---
+++ localbrew.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+set -eu
+
+HOMEBREW_NO_ENV_HINTS=1; export HOMEBREW_NO_ENV_HINTS
+
+test -d "${HOME:?}/.localbrew/.git" ||
+  git clone --depth=1 "https://github.com/Homebrew/brew" \
+    "${HOME:?}/.localbrew"
+
+test -d "${HOME:?}/.localbrew/.git" ||
+  { printf '%s\n' "Error: No ${HOME:?}/.localbrew repository!"; exit 1; }
+
+BREWSHELL="${HOME:?}/.localbrew/bin/bash"
+test -x "${BREWSHELL:?}" || BREWSHELL="/bin/sh"; export BREWSHELL
+
+# shellcheck disable=SC2016
+command -p env -i            \
+  HOME="${HOME:?}"           \
+  TERM="${TERM:?}"           \
+  BREWSHELL="${BREWSHELL:?}" \
+  HOMEBREW_NO_ENV_HINTS=1    \
+  "$(command -v sh)" -c '
+eval "$("${HOME:?}/.localbrew/bin/brew" shellenv)" ||
+  { printf "%s\n" "Error: Failed to setup brew environment!"; exit 1; }
+
+printf "%s\n" "$("${HOME:?}/.localbrew/bin/brew" --prefix)" |
+  grep -E "(/sw|/usr/local|/usr/opt|/opt)" &&
+    { printf "%s\n" "Error: Unexpected Homebrew prefix!"; exit 1; }
+
+	printf "\r%s\r" "* Updating brew ..."
+"${HOME:?}/.localbrew/bin/brew" update --force --quiet
+"${HOME:?}/.localbrew/bin/brew" install bash 2> /dev/null
+
+chmod -R go-w                                             \
+  "$("${HOME:?}/.localbrew/bin/brew" --prefix)"/share/zsh \
+    > /dev/null 2>&1
+
+BREWPATH="$("${HOME:?}/.localbrew/bin/brew" --prefix)/bin"
+POSIXPATH="$(command -p getconf PATH)"
+
+printf "%s\n" "${POSIXPATH:?}" |
+  grep -E "(/sw|/usr/local|/usr/opt|/opt)" &&
+    { printf "%s\n" "Error: Bad POSIXPATH: ${POSIXPATH:?}"; exit 1; }
+
+printf "[localbrew] Using Homebrew prefix: %s\n" \
+  "$("${HOME:?}/.localbrew/bin/brew" --prefix)"
+
+printf "[localbrew] Using PATH: %s\n" \
+  "${BREWPATH:?}:${POSIXPATH:?}"
+
+command -p exec env -i                \
+  HOME="${HOME:?}"                    \
+  PATH="${BREWPATH:?}:${POSIXPATH:?}" \
+  TERM="${TERM:?}"                    \
+  PS1="[localbrew] \s-\v\$ "          \
+  ${BREWSHELL:?}
+'
