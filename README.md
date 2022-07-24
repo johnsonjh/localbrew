9538fca99ac296d7a9573e20790d35028754afdc
Jeffrey H. Johnson <trnsz@pobox.com>
2022-07-24 00:25:32 -0400

    Relicense to 0BSD

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
+<!-- vim: set ft=markdown ts=4 sw=4 tw=0 expandtab colorcolumn=80 :         -->
+<!-- SPDX-License-Identifier: 0BSD                                          -->
+<!--                                                                        -->
+<!-- Copyright (c) 2022 Jeffrey H. Johnson ❬trnsz@pobox.com❭                -->
+<!--                                                                        -->
+<!-- Permission to use, copy, modify, and/or distribute this software for   -->
+<!-- any purpose with or without fee is hereby granted.                     -->
+<!--                                                                        -->
+<!-- THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL          -->
+<!-- WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED          -->
+<!-- WARRANTIES OF MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL THE      -->
+<!-- AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL   -->
+<!-- DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR  -->
+<!-- PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER         -->
+<!-- TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR       -->
+<!-- PERFORMANCE OF THIS SOFTWARE.                                          -->
+<!--                                                                        -->
 # localbrew
 
-Creates an isolated Homebrew for testing purposes
+Creates a (mostly) isolated Homebrew environment for testing purposes.
--- localbrew.sh
+++ localbrew.sh
@@ -1,17 +1,23 @@
 #!/bin/sh
 # vim: set ts=2 sw=2 tw=0 expandtab colorcolumn=80 :
-# SPDX-License-Identifier: FSFAP
+# SPDX-License-Identifier: 0BSD
 
-########################################################################
-#                                                                      #
-# Copyright (c) 2022 Jeffrey H. Johnson <trnsz@pobox.com>              #
-#                                                                      #
-# Copying and distribution of this file, with or without modification, #
-# are permitted in any medium without royalty provided the copyright   #
-# notice and this notice are preserved.  This file is offered "AS-IS", #
-# without any warranty.                                                #
-#                                                                      #
-########################################################################
+###############################################################################
+#
+# Copyright (c) 2022 Jeffrey H. Johnson <trnsz@pobox.com>
+#
+# Permission to use, copy, modify, and/or distribute this software for any
+# purpose with or without fee is hereby granted.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
+###############################################################################
 
 set -eu
 
